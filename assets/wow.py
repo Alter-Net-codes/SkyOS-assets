f08a5703d70cb1870b16bea98c927de387719c8e
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-15 14:42:59 -0700

    new app structure

--- assets/wow.py
+++ assets/wow.py
@@ -1,5 +1,3 @@
-import subprocess
-import os
 import sys
 
 def main():
@@ -7,21 +5,7 @@ def main():
     print("where the print statments are!")
     for i in range(5):
       print("wow!")
-    
-    # Path to the kernel script in the KERNEL folder
-    kernel_script = os.path.join(os.getcwd(), 'KERNEL', 'kernel.py')
-    
-    # Check if the kernel script exists before trying to run it
-    if os.path.isfile(kernel_script):
-        try:
-            # Run the kernel script as a subprocess
-            subprocess.run([sys.executable, kernel_script], check=True)
-        except subprocess.CalledProcessError as e:
-            print(f"Error executing the kernel script: {e}")
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-    else:
-        print("Kernel Script not found.")
+    sys.exit()
 
 if __name__ == "__main__":
     main()
